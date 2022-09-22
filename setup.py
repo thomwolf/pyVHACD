bd1cf4b5788ffc4157c82f1622661e6fc10d9576
Thomwolf <thomwolf@gmail.com>
2022-09-22 11:08:44 +0200

    move to v0.02

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pybind11 import get_cmake_dir
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 # The main interface is through Pybind11Extension.
 # * You can add cxx_std=11/14/17, and then build_ext can be removed.

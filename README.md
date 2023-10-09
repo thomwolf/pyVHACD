bfdcc0db49c3b8c9d64ed81b36ca2db12bfd53e8
Michael Dawson-Haggerty <mikedh@kerfed.com>
2023-10-09 14:50:43 -0400

    move metadata into pyproject

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 Python bindings for V-HACD
 
 A very simple and raw python binding for [V-HACD](https://github.com/kmammou/v-hacd) that is forked from [thomwolf/pyVHACD](https://github.com/thomwolf/pyVHACD) which generates an approximate convex decomposition of a triangle mesh.
-lated mesh.
 
 Contains a single method: `output = compute_vhacd(points, faces)` which take as inputs:
 - points: a (N, 3) Numpy array of double containing the coordinates of the N vertex of the mesh
--- setup.py
+++ setup.py
@@ -1,39 +1,28 @@
 # Available at setup time due to pyproject.toml
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
+import os
 
-__version__ = "0.0.3"
+# get version from pyproject.toml to pass in to build macro
+with open(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "pyproject.toml"))
+) as f:
+    __version__ = next(L.split("=")[1].strip().strip('"') for L in f if "version" in L)
+
 
 # The main interface is through Pybind11Extension.
 # * You can add cxx_std=11/14/17, and then build_ext can be removed.
 # * You can set include_pybind11=false to add the include directory yourself,
 #   say from a submodule.
-#
-# Note:
-#   Sort input source files if you glob sources to ensure bit-for-bit
-#   reproducible builds (https://github.com/pybind/python_example/pull/53)
-
-ext_modules = [
-    Pybind11Extension(
-        "vhacdx",
-        ["src/vhacdx/main.cpp"],
-        # Example: passing in the version to the compiled code
-        define_macros=[("VERSION_INFO", __version__)],
-    ),
-]
 
 setup(
-    name="vhacdx",
-    version=__version__,
-    author="Thomas Wolf",
-    author_email="thomas@huggingface.co",
-    url="https://github.com/trimesh/vhacdx",
-    description="Python bindings for VHACD",
-    long_description="Python bindings for VHACD",
-    ext_modules=ext_modules,
-    extras_require={"test": ["pytest", "numpy"]},
-    install_requires=["pybind11>=2.2", "numpy"],
+    ext_modules=[
+        Pybind11Extension(
+            "vhacdx",
+            ["src/vhacdx/main.cpp"],
+            # Example: passing in the version to the compiled code
+            define_macros=[("VERSION_INFO", __version__)],
+        ),
+    ],
     cmdclass={"build_ext": build_ext},
-    zip_safe=False,
-    python_requires=">=3.6",
 )
